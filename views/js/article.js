1443f36b417bcaa6fd7203d918328a7d08ae0a5a
qK1e <potapov988@yandex.ru>
2019-09-04 20:00:30 +0700

    Comments are done. Deleting comments in progress

--- views/js/article.js
+++ views/js/article.js
@@ -1,8 +1,34 @@
 function init() {
     $("#send-comment-btn").click(sendComment);
+    $("#comment-form").focusout(resetCommentForm);
+    $(".delete-comment").click(deleteComment);
 
     refreshCommentSection();
     setInterval(refreshCommentSection, 1000*60);
+}
+
+function deleteComment(event) {
+    event.stopPropagation();
+    alert("DELETING COMMENT!");
+}
+
+function resetCommentForm() {
+    $(this).find("#answer-to").val(0);
+    $(this).find("#comment-textarea").val("");
+}
+
+function reply() {
+    $("HTML, BODY").animate({
+        scrollTop: $("#comment-form").offset().top - 500
+    },
+    500);
+
+    $("#comment-textarea").focus();
+
+    let comment_id = $(this).find(".comment-id").html();
+    let login = $(this).find(".login").html();
+    $("#answer-to").val(comment_id);
+    $("#comment-textarea").val(login + ", ");
 }
 
 function sendComment()
@@ -23,7 +49,7 @@ function sendComment()
             dataType: "json",
             success: function (json) {
                 let text = $("#comment-textarea").val("");
-                alert("Comment posted successfully!");
+                refreshCommentSection();
             },
             error: function () {
                 alert("Error occurred!");
@@ -34,6 +60,7 @@ function sendComment()
 
 function refreshCommentSection()
 {
+
     let blog_id = $("#blog-id").val();
 
     $.ajax(
@@ -43,6 +70,7 @@ function refreshCommentSection()
             dataType: "html",
             success: function (html) {
                 $("#comment-section").html(html);
+                $(".comment").click(reply);
             }
         });
 }
